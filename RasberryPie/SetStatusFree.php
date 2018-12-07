85c5046c31bc866ccf9b98207f8f12f47b884845
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-07 11:36:01 +0900

    Add files via upload

---
+++ RasberryPie/SetStatusFree.php
@@ -0,0 +1,20 @@
+<?php
+/*
+※コードについて
+###現在の###トイレの状態をセットするPHPファイルです。     ###空室###
+URLを開くと自動で状態がセットされます。
+※注意
+Common.phpとこのファイルは同一ディレクトリに保存してください。
+*/
+require_once "Common.php";      //～～おまじない～～
+$db = new Common();             //
+
+$sql = 'UPDATE "ToiletTerminal" SET"Status" =  0';      //DBManagerからSQL文が決まったらここに入力！
+
+if($status !== 0){   //在室ならDBを操作しない（誤作動の可能性を考慮）
+    $sql = "UPDATE "ToiletTerminal" SET"Status" =  0";      //DBManagerからSQL文が決まったらここに入力！
+    $db->db_sql($sql);    //状態のセット実行
+}
+
+$db->db_close();
+?>
