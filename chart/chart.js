926236f564a0482b72b0748dfd8b2c0350f2ff7e
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-13 15:40:41 +0900

    Update chart.js

--- chart/chart.js
+++ chart/chart.js
@@ -47,15 +47,12 @@ function drawChart(){
     var option1 = {
               title : '2ヶ月累積データ',
              series: {
-              0:{targetAxisIndex:0},     // 第1系列は左のY軸を使用
-              1:{targetAxisIndex:1,
-               type: "line"},         // 第2系列は右のY時を使用
+              0:{targetAxisIndex:0}    // 第1系列は左のY軸を使用
              },
              hAxis: {title: '時間帯'},
              vAxes: {
                // 0:左のY軸。1:右のY軸
-               0: {title: '回数'},
-               1: {title: '時間[分]'}
+               0: {title: '回数'}
              },
            };
     
