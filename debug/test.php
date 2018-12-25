5ed55a7cf1a0fe0c0662c21815aeba95c575ec87
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-25 14:53:34 +0900

    Update test.php

--- debug/test.php
+++ debug/test.php
@@ -22,8 +22,8 @@ $result = $db->db_sql($sql);  //二次元配列を取得
 
 if(count($result) == 1)       //1レコードを想定しているのでそれ以外は排除
  {           
-  //$arr = $result[0];               //二次元配列から配列を取り出し、一次元配列にする。
-  $status = $result['Status'];        //使用状況（int）の値を取り出す。
+  $arr = $result[0];               //二次元配列から配列を取り出し、一次元配列にする。
+  $status = $arr['Status'];        //使用状況（int）の値を取り出す。
  }
 $db->db_close();
 
