5e2da582d9e6a437ad52e78deda5f47d7e905757
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-25 14:35:59 +0900

    Update ExportDumpDaily.php

--- debug/ExportDumpDaily.php
+++ debug/ExportDumpDaily.php
@@ -2,7 +2,7 @@
 require_once "./../php/Common.php";
 $db = new Common();
 
-$sql = "select * from \"ToiletTerminal\"";
+$sql = "select /"Status/" from \"ToiletTerminal\"";
 
 var_dump($db->db_sql($sql));    //ダンプファイルを表示
 
