16c086520409bab2926f85bd63dbb9e04af58f65
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-26 14:02:06 +0900

    Update debug.php

--- debug/debug.php
+++ debug/debug.php
@@ -9,7 +9,7 @@ if(isset($_POST['sql'])){
     $str = "\n\n\nAJAX REQUEST SUCCESS\nSQL:".$sql."\n";
     
     $dump = "\n".var_dump($db->db_sql($sql));
-    if($dump == null){
+    if($dump == "NULL"){
         $dump = "該当データなし";
     }
     $result = nl2br($str);
