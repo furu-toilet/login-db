ce9fe7ada2c8680212829ade0ebd122d64202cfd
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-26 13:48:10 +0900

    Add files via upload

---
+++ debug/debug.php
@@ -0,0 +1,20 @@
+<?php
+require_once "./Common.php";
+$db = new Common();
+
+header('Content-type: text/plain; charset= UTF-8');
+
+if(isset($_POST['sql'])){
+    $sql = $_POST['sql'];
+    $str = "AJAX REQUEST SUCCESS\nSQL:".$sql."\n";
+    
+    $dump = "\n".var_dump($db->db_sql($sql));
+    $result = nl2br($str);
+    echo $result;
+    echo $dump;
+}else{
+    echo 'FAIL TO AJAX REQUEST';
+}
+
+
+?>
