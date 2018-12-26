929c3c0cfbdde97dedc6fc9d570b84eba10c26e2
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-26 13:52:25 +0900

    Update debug.php

--- debug/debug.php
+++ debug/debug.php
@@ -6,7 +6,7 @@ header('Content-type: text/plain; charset= UTF-8');
 
 if(isset($_POST['sql'])){
     $sql = $_POST['sql'];
-    $str = "AJAX REQUEST SUCCESS\nSQL:".$sql."\n";
+    $str = "\nAJAX REQUEST SUCCESS\nSQL:".$sql."\n";
     
     $dump = "\n".var_dump($db->db_sql($sql));
     $result = nl2br($str);
