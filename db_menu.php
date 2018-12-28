ce93794c7757538ff01f8b9eb9dbbb2d83066088
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-12-28 16:45:11 +0900

    Update db_menu.php

--- db_menu.php
+++ db_menu.php
@@ -1,7 +1,9 @@
 <?php
-session_start();
 $result = null;
 $msg = null;
+$sql = $_POST['sql'];
+
+header('Content-type: text/plain; charset= UTF-8');
 
 if(isset($_POST['sql_submit'])){    //実行ボタン後の処理
 	require "./php/Common.php";
@@ -14,64 +16,35 @@ if(isset($_POST['sql_submit'])){    //実行ボタン後の処理
 }
 
 function h($str){                   //HTMLに文字列出力
-	return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+	//return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+	echo $str;
 }
 
 ?>
 
-<!DOCTYPE html>
-<html>
-  	<head>
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1">
-	    <title>PSMS  PostgreSQLManegmentStudio</title>
-	    <link rel="stylesheet" href="./css/style_db_menu.css">
-  	</head>
-<body>
-	<header>
-	<h1>PSMS  PostgreSQLManegmentStudio</h1>
-        <input class="left-in" type="submit" name="info_menu" value="ユーザ情報" onclick="location.href='./php/info.php'">
-	</header>
-    	<iframe class="leftframe" src="left.php"></iframe>
-    	<div class="right">
-	   <form method="post" name="SQL_sendform">
-	      <h2>SQL</h2>
-		<textarea class="sendarea" name="sql"></textarea><br>
-		<div class="right-inputarea">
-			<input class="right-in" type="submit" value="実行" name="sql_submit" id="sql_submit">
-			<input class="right-in" type="reset" value="リセット" name="sql_reset" id="sql_reset">
-		</div>
-		</form>
-      <div id="log">
-        <h2>実行結果</h2>
 
-<?php if(!empty($_POST['sql'])){           //SQL実行した場合?>       
-          <?=h('実行SQL：' . $_POST['sql'])?>	
+if(!empty($sql)){           //SQL実行した場合       
+          echo "実行SQL：" . $sql;
 	  <br>
-    <?php if($db->db_msg() == null){       //エラー時の表示を制御  ?>
-	    <?=h('正常終了') ?><br>
+    if($db->db_msg() == null){       //エラー時の表示を制御
+	    echo "正常終了"; <br>
             <table>
             <tr>
-        <?php foreach($result[0] as $key => $_): ?>
-            <th><?=h($key) ?></th>
-        <?php endforeach; ?>
+        foreach($result[0] as $key => $_){
+            <th> echo $key; </th>
+        }
             </tr>
-        <?php foreach($result as $values): ?>
+        foreach($result as $values){
             <tr>
-                <?php foreach($values as $value): ?>
-                    <td><?=h($value)?></td>
-                <?php endforeach; ?>
+                foreach($values as $value):
+                    <td> echo $value; </td>
+                }
             </tr>
-        <?php endforeach; ?>
+        }
             </table>
 
-    <?php }else{  //エラーメッセージ ?>	  
-          <?= h($db->db_msg()) ?>	     
-    <?php }   //else end  ?>
+    }else{  //エラーメッセージ	  
+          echo $db->db_msg();	     
+    }   //else end
           
-<?php } ?>          
-		</div>
-</div>
-
-</body>
-</html>
+}  
