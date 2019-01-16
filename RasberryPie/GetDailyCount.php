d0fbcbbecbfc62c3f030b741140c117f3901a2db
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-16 15:26:17 +0900

    Update GetDailyCount.php

--- RasberryPie/GetDailyCount.php
+++ RasberryPie/GetDailyCount.php
@@ -51,7 +51,7 @@ foreach($result as $i)      //0:00～23:00までのデータを格納する。
     {
         if($list[0] == ($icount - 1).":00")    //$dayの時間帯を参照し、対応する部分にデータを格納する
         {                
-            $result[$icount]=$list;
+            $result[$count]=$list;
             break;        //データを格納した場合、ループを抜ける
         }            
         $jcount++;
