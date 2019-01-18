45e7ab70eea3137917a8db8a202a186d12516c9b
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-18 17:20:01 +0900

    Update GetWeeklyTime.php

--- RasberryPie/GetWeeklyTime.php
+++ RasberryPie/GetWeeklyTime.php
@@ -1,33 +1,44 @@
 <?php
 /*
-GetWeeklyTime.phpについて
-1日ごとのトイレ使用時間を戻り値として返すphp
+GetDailyCount.phpについて
+1日ごとのトイレ使用回数を戻り値として返すphp
 グラフでの仕様なので戻り値は「print」関数にて返す。
 ※形式はjson
-※配列
+*/
+
+
+require_once "Common.php";      //～～おまじない～～
+$db = new Common();             //
+$result = array();
+$timezone = 24;   //グラフのメモリを何時まで表示するか決める。
+
+
+/*グラフ用データの土台を作成する    
 
 ex）[
-    ['日','回数'],
-    [8:00,  2],
-    [9:00,  5]    
-    ]
-
+    ['時間帯','使用回数'],
+    [0:00,  0],
+    [1:00,  0],
+         ・
+         ・
+         ・
+    [23:00, 0]
 */
-require_once "Common.php";
-$db = new Common();
 
+
+//今日の時間帯ごとの使用回数を求めるSQLを格納する。
 $sql = "
-SELECT EXTRACT (HOUR FROM \"StartTime\")  ||':00',sum(\"UsedTime\")
+SELECT EXTRACT (HOUR FROM \"StartTime\")  ||':00',COUNT(*)
 FROM \"RuiInfo\"
+WHERE CAST(\"StartTime\" as DATE) = CAST(CURRENT_TimeStamp + '9 hours'  as DATE)
 GROUP BY EXTRACT(HOUR FROM \"StartTime\")
-ORDER BY EXTRACT(HOUR FROM \"StartTime\") ;
+ORDER BY EXTRACT(HOUR FROM \"StartTime\");
 ";      //DBManagerからSQL文が決まったらここに入力！
+$daycount = $db->db_sql($sql);    //二次元配列を取得。　値は 時間帯(Str型),使用回数(Int型)
 
-$daysum = $db->db_sql($sql);    //連想配列を取得   値は時間帯(Str型),利用時間(int型)
+array_push($result,array("時間帯","使用回数"));
 
-array_push($result,array("時間帯","使用時間"));
-
-for($i=0;$i<$timezone;$i++)     //時間帯と使用時間 0をセットする
+for($i=0;$i<$timezone;$i++)     //時間帯と使用回数 0をセットする
 {
     array_push($result,[$i.":00",0]);
 }
@@ -39,15 +50,13 @@ foreach($result as $i)      //0:00～23:00までのデータを格納する。  
     {
         if(strcmp($list['?column?'],$icount.":00") == 0)    //$listの時間帯を参照し、対応する部分にデータを格納する
         {                
-            $result[$icount + 1][1] = $list['sum'];
+            $result[$icount + 1][1] = $list['count'];
             break;        //データを格納した場合、ループを抜ける
         }            
     }
     $icount++;
 } 
 
-$db->db_close();
-
-echo json_encode($result);
+echo json_encode( $result);//jsonで使用する型に変換する。
 
 ?>
