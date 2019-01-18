1afc650c5060b27b67cf8eb6be5791652ef483b8
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-18 17:45:46 +0900

    Update GetWeeklyCount.php

--- RasberryPie/GetWeeklyCount.php
+++ RasberryPie/GetWeeklyCount.php
@@ -1,10 +1,9 @@
 <?php
 /*
 GetWeeklyCount.phpについて
-1週間ごとの1日当たりのトイレ使用回数を戻り値として返すphp
+1週間の時間帯当たりのトイレ使用回数を戻り値として返すphp
 グラフでの仕様なので戻り値は「print」関数にて返す。
 ※形式はjson
-※配列
 
 ex）[
     ['日','回数'],
@@ -12,38 +11,41 @@ ex）[
     [26,  37]    
     [27,  22]    
     ]
-
 */
-
 require_once "Common.php";      //～～おまじない～～
 $db = new Common();             //
 $result = array();
-array_push($result,array("日付","使用回数"));
+$timezone = 24;   //グラフのメモリを何時まで表示するか決める。
 
-for($i=6;$i>-1;$i--)
+//一週間の時間帯ごとの使用回数を求めるSQLを格納する。
+$sql = "
+SELECT EXTRACT (HOUR FROM \"StartTime\")  ||':00',COUNT(*)
+FROM \"RuiInfo\"
+GROUP BY EXTRACT(HOUR FROM \"StartTime\")
+ORDER BY EXTRACT(HOUR FROM \"StartTime\");
+";      //DBManagerからSQL文が決まったらここに入力！
+$weekcount = $db->db_sql($sql);    //二次元配列を取得。　値は 時間帯(Str型),使用回数(Int型)
+
+array_push($result,array("時間帯","使用回数"));
+
+for($i=0;$i<$timezone;$i++)     //時間帯と使用回数 0をセットする
 {
-	array_push($result,array(date(j,strtotime("-".$i."day")),0));	//日付と0を配列に加える。
+    array_push($result,[$i.":00",0]);
 }
 
-$count = 6;
-
-foreach($result as $a)
-{
-	$sql = "
-        	SELECT EXTRACT (Day FROM \"Date\"),count(*) 
-       		FROM \"RuiInfo\"
-		    WHERE \"Date\" = CURRENT_Date - ".($count+1). 
-        	"GROUP BY \"Date\";";
-	$arr = $db->db_sql($sql);    //連想配列を取得   値は値は　日付(str型)と使用時間（int型）
-	
-	if($arr[0][0] == date(j,strtotime("-".($count+1)."day")))//使用データがあった場合、対応する日付の部分にデータを格納する
-	 {
-		$a[8-$count]=$arr[0];
-	 }
-	
-	$count--;
-}
-
+$icount = 0;
+foreach($result as $i)      //0:00～23:00までのデータを格納する。    i
+{    
+    foreach($weekcount as $list)    //$weekcountのデータの数だけforeach文を回す    j
+    {
+        if(strcmp($list['?column?'],$icount.":00") == 0)    //$listの時間帯を参照し、対応する部分にデータを格納する
+        {                
+            $result[$icount + 1][1] = $list['count'];
+            break;        //データを格納した場合、ループを抜ける
+        }            
+    }
+    $icount++;
+} 
 
 $db->db_close();
 
