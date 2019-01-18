efd558b6495808488fc443bc6acff763bd9f259e
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-18 17:32:07 +0900

    Update GetWeeklyTime.php

--- RasberryPie/GetWeeklyTime.php
+++ RasberryPie/GetWeeklyTime.php
@@ -1,44 +1,37 @@
 <?php
 /*
-GetDailyCount.phpについて
-1日ごとのトイレ使用回数を戻り値として返すphp
+GetWeeklyTime.phpについて
+1日ごとのトイレ使用時間を戻り値として返すphp
 グラフでの仕様なので戻り値は「print」関数にて返す。
 ※形式はjson
-*/
+※配列
 
+ex）[
+    ['日','回数'],
+    [8:00,  2],
+    [9:00,  5]    
+    ]
+
+*/
 
 require_once "Common.php";      //～～おまじない～～
 $db = new Common();             //
 $result = array();
 $timezone = 24;   //グラフのメモリを何時まで表示するか決める。
 
-
-/*グラフ用データの土台を作成する    
-
-ex）[
-    ['時間帯','使用回数'],
-    [0:00,  0],
-    [1:00,  0],
-         ・
-         ・
-         ・
-    [23:00, 0]
-*/
-
-
-//今日の時間帯ごとの使用回数を求めるSQLを格納する。
+//一週間の時間帯ごとの使用回数を求めるSQLを格納する。
 $sql = "
-SELECT EXTRACT (HOUR FROM \"StartTime\")  ||':00',COUNT(*)
+SELECT EXTRACT (HOUR FROM \"StartTime\")  ||':00',sum(\"UsedTime\")
 FROM \"RuiInfo\"
-WHERE CAST(\"StartTime\" as DATE) = CAST(CURRENT_TimeStamp + '9 hours'  as DATE)
 GROUP BY EXTRACT(HOUR FROM \"StartTime\")
 ORDER BY EXTRACT(HOUR FROM \"StartTime\");
 ";      //DBManagerからSQL文が決まったらここに入力！
-$daycount = $db->db_sql($sql);    //二次元配列を取得。　値は 時間帯(Str型),使用回数(Int型)
 
-array_push($result,array("時間帯","使用回数"));
+$daysum = $db->db_sql($sql);    //連想配列を取得   値は時間帯(Str型),利用時間(int型)
 
-for($i=0;$i<$timezone;$i++)     //時間帯と使用回数 0をセットする
+array_push($result,array("時間帯","使用時間"));
+
+for($i=0;$i<$timezone;$i++)     //時間帯と使用時間 0をセットする
 {
     array_push($result,[$i.":00",0]);
 }
@@ -50,13 +43,15 @@ foreach($result as $i)      //0:00～23:00までのデータを格納する。  
     {
         if(strcmp($list['?column?'],$icount.":00") == 0)    //$listの時間帯を参照し、対応する部分にデータを格納する
         {                
-            $result[$icount + 1][1] = $list['count'];
+            $result[$icount + 1][1] = $list['sum'];
             break;        //データを格納した場合、ループを抜ける
         }            
     }
     $icount++;
 } 
 
-echo json_encode( $result);//jsonで使用する型に変換する。
+$db->db_close();
+
+echo json_encode($result);
 
 ?>
