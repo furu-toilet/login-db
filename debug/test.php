76671068df546a3c878b9a5121ffe124a56c5c2a
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-26 16:02:49 +0900

    Update test.php

--- debug/test.php
+++ debug/test.php
@@ -1,36 +1,37 @@
 <?php
-
-$result = array();        //グラフへ渡す結果が格納される。
-$title  = ["時間帯","使用回数","時間-分"];    //データ名称を配列に格納する。
-
 require_once "../php/Common.php";
-$db  = new Common();
-$sql = "SELECT EXTRACT (HOUR FROM \"StartTime\") ||':00' as 時間帯,COUNT(*) 使用回数 ,sum(\"UsedTime\")時間 
-FROM \"RuiInfo\" 
-WHERE CAST(\"StartTime\" as DATE) = '2018-12-27' 
-GROUP BY EXTRACT(HOUR FROM \"StartTime\") 
-ORDER BY EXTRACT(HOUR FROM \"StartTime\");";
-
-    $day = $db->db_sql($sql);
+$db = new Common();
+$result = null;
+header('Content-type: text/plain; charset= UTF-8');
+if(isset($_POST['sql'])){
+    $sql = $_POST['sql'];
+    $str = "\n\n\nAJAX REQUEST SUCCESS\nSQL:".$sql."\n";
     
-    array_push($result,$title);//データ名称を格納する
-
-    for($i=0;$i<24;$i++)      //0:00～23:00までのデータを格納する。
-    {
-        for($j=0;$j<count($day);$j++)    //$dayのデータの数だけfor文を回す
-        {
-            if($day[$j][0] == $i.":00")    //$dayの時間帯を参照し、対応する部分にデータを格納する
-            {
-                array_push($result,[$day[$j][0],$day[$j][1],$day[$j][2]]);
-                break;        //データを格納した場合、ループを抜ける
-            }
-            else if($j==count($day)-1)    //対応するデータがなかった場合、時間帯と0,0を格納する
-            {
-                array_push($result,[$i.":00",0,0]);
-            }
-        }
+    $data = $db->db_sql($sql);
+    
+    if($data == null){
+        $dump = "該当データなし";
+    }else {
+        //$dump = "\n".var_dump($data);
+        //$dump = var_dump($data);
     }
+    //$result = nl2br($str);
+    //echo $result;
+    echo arr_change($data);
+}else{
+    echo 'FAIL TO AJAX REQUEST';
+}
 
-   var_dump($result);
+function arr_change($arr){
+    $result = null;
+    $cnt = 0;
+    $clm = null;
+    foreach($arr as $column){
+        $clm = array_push($column[0]);
+    }
+    $result = $clm;
+    
+    return $result
+}
 
 ?>
