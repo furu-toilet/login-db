3ae1a36f17e21567fcc1a4f1e271ccd90bbe9563
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-26 16:05:43 +0900

    Update test.php

--- debug/test.php
+++ debug/test.php
@@ -3,8 +3,9 @@ require_once "../php/Common.php";
 $db = new Common();
 $result = null;
 header('Content-type: text/plain; charset= UTF-8');
-if(isset($_POST['sql'])){
-    $sql = $_POST['sql'];
+//if(isset($_POST['sql'])){
+    //$sql = $_POST['sql'];
+    $sql = "select * from user_info";
     $str = "\n\n\nAJAX REQUEST SUCCESS\nSQL:".$sql."\n";
     
     $data = $db->db_sql($sql);
@@ -19,9 +20,9 @@ if(isset($_POST['sql'])){
     //echo $result;
     //echo arr_change($data);
     var_dump(arr_change($data));
-}else{
-    echo 'FAIL TO AJAX REQUEST';
-}
+//}else{
+    //echo 'FAIL TO AJAX REQUEST';
+//}
 
 function arr_change($arr){
     $result = null;
