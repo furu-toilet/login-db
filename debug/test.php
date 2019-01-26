5fa45b50c02542753b3581dd7b952e676960e289
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-26 16:14:58 +0900

    Update test.php

--- debug/test.php
+++ debug/test.php
@@ -7,17 +7,15 @@ $sql = "select * from user_info";
 
 $data = $db->db_sql($sql);
 
-var_dump(arr_change($data));
 
-function arr_change($arr){
-    $cnt = 0;
-    $clm = null;
-    foreach($arr as $column  => $_){
-        $clm = array_push($column);
-    }
-    $result = $clm;
-    
-    return $result;
+$cnt = 0;
+$clm = null;
+foreach($arr as $column  => $_){
+    $clm = array_push($column);
 }
+$result = $clm;
 
+
+var_dump($result);
+    
 ?>
