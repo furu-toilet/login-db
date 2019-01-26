bbd9f20d9711b01ebbf8e9f37f31aea2c4d0664f
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2019-01-26 16:13:16 +0900

    Update test.php

--- debug/test.php
+++ debug/test.php
@@ -2,27 +2,12 @@
 require_once "../php/Common.php";
 $db = new Common();
 $result = null;
-header('Content-type: text/plain; charset= UTF-8');
-//if(isset($_POST['sql'])){
-    //$sql = $_POST['sql'];
-    $sql = "select * from user_info";
-    $str = "\n\n\nAJAX REQUEST SUCCESS\nSQL:".$sql."\n";
-    
-    $data = $db->db_sql($sql);
-    
-    if($data == null){
-        $dump = "該当データなし";
-    }else {
-        //$dump = "\n".var_dump($data);
-        //$dump = var_dump($data);
-    }
-    //$result = nl2br($str);
-    //echo $result;
-    //echo arr_change($data);
-    var_dump(arr_change($data));
-//}else{
-    //echo 'FAIL TO AJAX REQUEST';
-//}
+$sql = "select * from user_info";
+
+
+$data = $db->db_sql($sql);
+
+var_dump(arr_change($data));
 
 function arr_change($arr){
     $cnt = 0;
